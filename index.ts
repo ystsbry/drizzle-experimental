e94062557671a4e9c5e8d7d6892b1fe54897f012
ystsbry <jmachang368@gmail.com>
2025-08-09 22:15:28 +0900

    feat: project initialize

---
+++ index.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env bun
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+  .name("drizzle-experimental-cli")
+  .description("Experimental CLI Tool for Drizzle")
+  .version("0.0.1");
+
+program
+  .command("hello <name>")
+  .description("Say hello to someone")
+  .action((name) => {
+    console.log(`Hello, ${name}!`);
+  });
+
+program.parse();
